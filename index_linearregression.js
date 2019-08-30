8a2b1ee751fd02429c253563852811c8e18dd78d
Venkata Chadalawada <venchad@gmail.com>
2019-08-29 20:15:05 -0500

    Update index_linearregression.js

--- index_linearregression.js
+++ index_linearregression.js
@@ -2,6 +2,7 @@ require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('.././load-csv');
 const LinearRegression = require('./linear-regression');
+const plot = require('node-remote-plot');
 
 let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
     shuffle: true,
@@ -11,14 +12,35 @@ let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
 });
 
 const regression = new LinearRegression(features, labels, {
-    learningRate: 1,
+    learningRate: 0.1,
     iterations: 100
 });
 
 regression.features.print();
 
 regression.train();
-regression.test(testFeatures, testLabels);
+const R2 = regression.test(testFeatures, testLabels);
+
+// console.log('-----MSE HISTORY----', regression.mseHistory);
+// console.log('-----B HISTORY----', regression.bHistory);
+
+console.log('----R2 is ---------', R2);
+
+plot({
+    x: regression.mseHistory.reverse(),
+    xLabel: 'Iteration#',
+    yLabel: 'Mean Squared Error'
+});
+
+
+plot({
+    x: regression.bHistory,
+    y: regression.mseHistory.reverse(),
+    xLabel: 'Iteration#',
+    yLabel: 'Mean Squared Error'
+});
+
+
 // console.log('Updated weights m , b is:', regression.weights.print());
 // with learningRate: 0.001, - -9 NOT GOOD AT ALL
 // with learningRate: 0.1, - ~60%
